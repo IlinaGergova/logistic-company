09b08ea445da6b0ee92dee8383cc5d08293847ab
Ivo Stefanov <ivostefanovtues@gmail.com>
2024-09-08 18:22:18 +0300

    remove unused role and old comments

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -23,16 +23,6 @@ export class JwtAuthGuard implements CanActivate {
     });
 
     request['user'] = payload;
-    // try {
-    //   const payload = await this.jwtService.verifyAsync(token, {
-    //     secret: jwtConstants.secret,
-    //   });
-
-    //   request['user'] = payload;
-    // } catch {
-    //   console.log("AAAAAAAAAAAAAAAAAAA")
-    //   throw new UnauthorizedException();
-    // }
     return true;
   }
 
@@ -41,7 +31,3 @@ export class JwtAuthGuard implements CanActivate {
     return type === 'Bearer' ? token : undefined;
   }
 }
-
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-// .eyJ1c2VybmFtZSI6ImFkbWluIiwiaWF0IjoxNzIwNjI1MjY0LCJleHAiOjE3NTIxNjEyNjR9
-// .xRCHUGSOjwYvcQIeBKtYa2ldnCGnFXjXHBPsxT3C-6s
